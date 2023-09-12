d28571a317d2d3ccc64317de696e6ba8e21f0248
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-09-12 21:41:42 +0200

    Add more test for syncing a DAG between scheduler and database

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 rm test.db
 sqlite3 test.db < schema.sql
 go generate
-go test ./...
+go test -count=1 ./...
 go build -o ./bin/scheduler ./cmd/scheduler
 go build -o ./bin/executor ./cmd/executor
--- cmd/scheduler/start_test.go
+++ cmd/scheduler/start_test.go
@@ -23,6 +23,7 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	// First sync
 	dagId := "very_simple_dag"
 	d := verySimpleDag(dagId)
+	tasksNum := len(d.Flatten())
 	s1Err := syncDag(c, d)
 	if s1Err != nil {
 		t.Fatalf("Unexpected error while syncDag: %s", s1Err.Error())
@@ -30,7 +31,7 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	}
 
 	// Checks after the first sync
-	dagtasksCountCheck(1, 3, c, t)
+	dagtasksCountCheck(1, tasksNum, c, t)
 
 	dagDb1, rErr := c.ReadDag(dagId)
 	if rErr != nil {
@@ -40,8 +41,8 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	if dtErr != nil {
 		t.Fatalf("Unexpected error while reading dag tasks from dagtasks table: %s", dtErr.Error())
 	}
-	if len(dagtasksDb1) != 3 {
-		t.Fatalf("Expected 3 dag tasks in dagtasks table, got: %d", len(dagtasksDb1))
+	if len(dagtasksDb1) != tasksNum {
+		t.Fatalf("Expected %d dag tasks in dagtasks table, got: %d", tasksNum, len(dagtasksDb1))
 	}
 
 	// Second sync - should not change anything
@@ -51,7 +52,7 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	}
 
 	// Checks after the second sync
-	dagtasksCountCheck(1, 3, c, t)
+	dagtasksCountCheck(1, tasksNum, c, t)
 
 	dagDb2, r2Err := c.ReadDag(dagId)
 	if r2Err != nil {
@@ -62,8 +63,8 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	if dt2Err != nil {
 		t.Fatalf("Unexpected error while reading dag tasks from dagtasks table: %s", dt2Err.Error())
 	}
-	if len(dagtasksDb2) != 3 {
-		t.Fatalf("Expected 3 dag tasks in dagtasks table, got: %d", len(dagtasksDb1))
+	if len(dagtasksDb2) != tasksNum {
+		t.Fatalf("Expected %d dag tasks in dagtasks table, got: %d", tasksNum, len(dagtasksDb1))
 	}
 
 	if dagDb1 != dagDb2 {
@@ -84,16 +85,137 @@ func TestSyncOneDagNoChanges(t *testing.T) {
 	}
 }
 
+func TestSyncOneDagChangingAttr(t *testing.T) {
+	c, err := db.NewInMemoryClient(sqlSchemaPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Before sync - db is empty
+	dagtasksCountCheck(0, 0, c, t)
+
+	// First sync
+	dagId := "very_simple_dag"
+	d := verySimpleDag(dagId)
+	tasksNum := len(d.Flatten())
+	s1Err := syncDag(c, d)
+	if s1Err != nil {
+		t.Fatalf("Unexpected error while syncDag: %s", s1Err.Error())
+		return
+	}
+
+	// Checks after the first sync
+	dagtasksCountCheck(1, tasksNum, c, t)
+
+	dagDb1, rErr := c.ReadDag(dagId)
+	if rErr != nil {
+		t.Fatalf("Unexpected error while reading dag from dags table: %s", rErr.Error())
+	}
+
+	// Update DAG attributes
+	d.Attr.Schedule = "8 8 * * 1-5"
+
+	// Second sync - should not change anything
+	s2Err := syncDag(c, d)
+	if s2Err != nil {
+		t.Fatalf("Unexpected error while the second syncDag: %s", s2Err.Error())
+	}
+
+	// Checks after the second sync
+	dagtasksCountCheck(1, tasksNum, c, t)
+
+	dagDb2, r2Err := c.ReadDag(dagId)
+	if rErr != nil {
+		t.Fatalf("Unexpected error while reading dag after the update from dags table: %s", r2Err.Error())
+	}
+	if dagDb1 == dagDb2 {
+		t.Fatal("Expected the row in dags table to be different after DAG's attr were updated")
+	}
+	if dagDb1.HashAttributes == dagDb2.HashAttributes {
+		t.Errorf("Expected different HashAttr after the updated, but is unchanged: %s", dagDb1.HashAttributes)
+	}
+	if dagDb1.Attributes == dagDb2.Attributes {
+		t.Errorf("Expected different Attributes after the updated, but is unchanged: %s", dagDb1.Attributes)
+	}
+	if dagDb1.HashTasks != dagDb2.HashTasks {
+		t.Errorf("Expected HashTasks to be unchanged after the update, got different - before: %s, after: %s",
+			dagDb1.HashTasks, dagDb1.HashTasks)
+	}
+}
+
+func TestSyncOneDagChangingTasks(t *testing.T) {
+	c, err := db.NewInMemoryClient(sqlSchemaPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Before sync - db is empty
+	dagtasksCountCheck(0, 0, c, t)
+
+	// First sync
+	dagId := "very_simple_dag"
+	d := verySimpleDag(dagId)
+	tasksNum := len(d.Flatten())
+	s1Err := syncDag(c, d)
+	if s1Err != nil {
+		t.Fatalf("Unexpected error while syncDag: %s", s1Err.Error())
+		return
+	}
+
+	// Checks after the first sync
+	dagtasksCountCheck(1, tasksNum, c, t)
+
+	dagDb1, rErr := c.ReadDag(dagId)
+	if rErr != nil {
+		t.Fatalf("Unexpected error while reading dag from dags table: %s", rErr.Error())
+	}
+
+	// Update DAG tasks
+	additionalTask := dag.Node{Task: tasks.PrintTask{Name: "bonus_task"}}
+	d.Root.Next(&additionalTask)
+
+	// Second sync - should not change anything
+	s2Err := syncDag(c, d)
+	if s2Err != nil {
+		t.Fatalf("Unexpected error while the second syncDag: %s", s2Err.Error())
+	}
+
+	// Checks after the second sync
+	dagtasksCountCheck(1, tasksNum+tasksNum+1, c, t)
+
+	currentDagTasks := c.CountWhere("dagtasks", "IsCurrent=1")
+	if currentDagTasks != tasksNum+1 {
+		t.Fatalf("Expected %d current rows in dagtasks table after the update, got: %d", tasksNum+1, currentDagTasks)
+	}
+
+	dagDb2, r2Err := c.ReadDag(dagId)
+	if rErr != nil {
+		t.Fatalf("Unexpected error while reading dag after the update from dags table: %s", r2Err.Error())
+	}
+	if dagDb1 == dagDb2 {
+		t.Fatal("Expected the row in dags table to be different after DAG's attr were updated")
+	}
+	if dagDb1.HashTasks == dagDb2.HashTasks {
+		t.Errorf("Expected different HashTasks after the update, but is unchanged: %s", dagDb1.HashTasks)
+	}
+	if dagDb1.HashAttributes != dagDb2.HashAttributes {
+		t.Errorf("Expected HashAttr to be unchanged after the update, got different - before: %s, after: %s",
+			dagDb1.HashAttributes, dagDb2.HashAttributes)
+	}
+	if dagDb1.Attributes != dagDb2.Attributes {
+		t.Errorf("Expected Attributes to be unchanged after the update, got different - before: %s, after: %s",
+			dagDb1.Attributes, dagDb2.Attributes)
+	}
+}
+
 func dagtasksCountCheck(expDagCnt, expDagTasksCnt int, c *db.Client, t *testing.T) {
 	dagCount := c.Count("dags")
 	if dagCount != expDagCnt {
-		t.Errorf("Expected %d row in dags table, got: %d", expDagCnt, dagCount)
-		return
+		t.Fatalf("Expected %d row in dags table, got: %d", expDagCnt, dagCount)
 	}
 	dagtasksCount := c.Count("dagtasks")
 	if dagtasksCount != expDagTasksCnt {
-		t.Errorf("Expected %d rows in dagtasks table, got: %d", expDagTasksCnt, dagtasksCount)
-		return
+		t.Fatalf("Expected %d rows in dagtasks table, got: %d", expDagTasksCnt, dagtasksCount)
 	}
 }
 
