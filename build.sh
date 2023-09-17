60a1199c563841e6053a8773611fa46908b062e2
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-09-17 17:06:04 +0200

    Add benchmarks for build script

--- build.sh
+++ build.sh
@@ -2,5 +2,7 @@ rm test.db
 sqlite3 test.db < schema.sql
 go generate
 go test -count=1 ./...
+go test -bench=. -benchmem ./src/meta
+go test -bench=. -benchmem ./src/sched
 go build -o ./bin/scheduler ./cmd/scheduler
 go build -o ./bin/executor ./cmd/executor
