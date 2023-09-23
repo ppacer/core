49e82111a66aae42cf62402543c2f4b930d22742
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-09-23 18:27:58 +0200

    Add ReadDagRuns

--- src/db/dagruns.go
+++ src/db/dagruns.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"go_shed/src/version"
 	"time"
 
 	"github.com/rs/zerolog/log"
@@ -12,11 +13,53 @@ type DagRun struct {
 	ExecTs   string
 	InsertTs string
 	Status   string
+	Version  string
 }
 
 const (
 	DagRunStatusScheduled = "SCHEDULED"
 )
+
+func (c *Client) ReadDagRuns(dagId string, topN int) ([]DagRun, error) {
+	start := time.Now()
+	log.Info().Str("dagId", dagId).Int("topN", topN).Msgf("[%s] Start reading DAG runs...", LOG_PREFIX)
+	cap := topN
+	if topN < 0 {
+		cap = 1000
+	}
+	dagruns := make([]DagRun, 0, cap)
+
+	rows, qErr := c.dbConn.Query(c.readDagRunsQuery(topN), dagId, topN)
+	if qErr != nil {
+		log.Error().Err(qErr).Str("dagId", dagId).Int("topN", topN).Msgf("[%s] Failed querying DAG runs.", LOG_PREFIX)
+		return nil, qErr
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var runId int64
+		var dagId, execTs, insertTs, status, version string
+
+		scanErr := rows.Scan(&runId, &dagId, &execTs, &insertTs, &status)
+		if scanErr != nil {
+			log.Error().Err(scanErr).Str("dagId", dagId).Msgf("[%s] Failed scanning a DagRun record.", LOG_PREFIX)
+			return nil, scanErr
+		}
+		dagrun := DagRun{
+			RunId:    runId,
+			DagId:    dagId,
+			ExecTs:   execTs,
+			InsertTs: insertTs,
+			Status:   status,
+			Version:  version,
+		}
+		dagruns = append(dagruns, dagrun)
+	}
+
+	log.Info().Str("dagId", dagId).Int("topN", topN).Dur("durationMs", time.Since(start)).
+		Msgf("[%s] Start reading DAG runs...", LOG_PREFIX)
+	return dagruns, nil
+}
 
 // InsertDagRun inserts new row into dagruns table for given DagId and execution timestamp. Initial status is set to
 // DagRunStatusScheduled. RunId for just inserted dag run is returned or -1 in case when error is not nil.
@@ -24,7 +67,7 @@ func (c *Client) InsertDagRun(dagId, execTs string) (int64, error) {
 	start := time.Now()
 	insertTs := time.Now().Format(InsertTsFormat)
 	log.Info().Str("dagId", dagId).Str("execTs", insertTs).Msgf("[%s] Start inserting dag run...", LOG_PREFIX)
-	res, err := c.dbConn.Exec(c.insertDagRunQuery(), dagId, execTs, insertTs, DagRunStatusScheduled)
+	res, err := c.dbConn.Exec(c.insertDagRunQuery(), dagId, execTs, insertTs, DagRunStatusScheduled, version.Version)
 	if err != nil {
 		log.Error().Err(err).Str("dagId", dagId).Str("execTs", execTs).Msgf("[%s] Cannot insert new dag run", LOG_PREFIX)
 		return -1, err
@@ -34,9 +77,44 @@ func (c *Client) InsertDagRun(dagId, execTs string) (int64, error) {
 	return res.LastInsertId()
 }
 
+func (c *Client) readDagRunsQuery(topN int) string {
+	if topN == -1 {
+		return `
+			SELECT
+				RunId,
+				DagId,
+				ExecTs,
+				InsertTs,
+				Status
+			FROM
+				dagruns
+			WHERE
+				DagId = ?
+			ORDER BY
+				RunId DESC
+		`
+	}
+	return `
+		SELECT
+			RunId,
+			DagId,
+			ExecTs,
+			InsertTs,
+			Status
+		FROM
+			dagruns
+		WHERE
+			DagId = ?
+		ORDER BY
+			RunId DESC
+		LIMIT
+			?
+	`
+}
+
 func (c *Client) insertDagRunQuery() string {
 	return `
-		INSERT INTO dagruns (DagId, ExecTs, InsertTs, Status)
-		VALUES (?, ?, ?, ?)
+		INSERT INTO dagruns (DagId, ExecTs, InsertTs, Status, Version)
+		VALUES (?, ?, ?, ?, ?)
 	`
 }
--- src/db/dagruns_test.go
+++ src/db/dagruns_test.go
@@ -38,3 +38,147 @@ func TestInsertDagRunSimple(t *testing.T) {
 		t.Errorf("Expected 2 row got: %d", c2)
 	}
 }
+
+func TestInsertAndReadDagRunsAll(t *testing.T) {
+	c, err := NewInMemoryClient(sqlSchemaPath)
+	if err != nil {
+		t.Error(err)
+	}
+	dagId := "mock_dag"
+	timestamps := []string{
+		"2023-09-23T10:10:00",
+		"2023-09-23T10:20:00",
+		"2023-09-23T10:30:00",
+		"2023-09-23T10:40:00",
+		"2023-09-23T10:50:00",
+		"2023-09-23T11:00:00",
+	}
+	for _, ts := range timestamps {
+		insertDagRun(c, dagId, ts, t)
+	}
+
+	// Read all dag runs for mock_dag
+	dagRunsAll, rErr := c.ReadDagRuns(dagId, -1)
+	if rErr != nil {
+		t.Fatalf("Error while reading all dag runs for %s: %s", dagId, rErr.Error())
+	}
+
+	if len(dagRunsAll) != len(timestamps) {
+		t.Errorf("Expected %d dag runs, got %d", len(timestamps), len(dagRunsAll))
+		for _, dr := range dagRunsAll {
+			t.Logf("%+v", dr)
+		}
+	}
+
+	rows := len(dagRunsAll)
+	for idx, dr := range dagRunsAll {
+		if dr.RunId != int64(rows-idx) {
+			t.Errorf("Expected RunId=%d, got %d", rows-idx, dr.RunId)
+		}
+		if dr.ExecTs != timestamps[rows-idx-1] {
+			t.Errorf("Expected ExecTs=%s, got: %s", timestamps[rows-idx-1], dr.ExecTs)
+		}
+		if dr.Status != DagRunStatusScheduled {
+			t.Errorf("Expected Status=%s, but got: %s", DagRunStatusScheduled, dr.Status)
+		}
+	}
+}
+
+func TestInsertAndReadDagRunsTop3(t *testing.T) {
+	c, err := NewInMemoryClient(sqlSchemaPath)
+	if err != nil {
+		t.Error(err)
+	}
+	dagId := "mock_dag"
+	timestamps := []string{
+		"2023-09-23T10:10:00",
+		"2023-09-23T10:20:00",
+		"2023-09-23T10:30:00",
+		"2023-09-23T10:40:00",
+		"2023-09-23T10:50:00",
+		"2023-09-23T11:00:00",
+	}
+	for _, ts := range timestamps {
+		insertDagRun(c, dagId, ts, t)
+	}
+
+	// Read dag runs for top 3 runs
+	const topN = 3
+	dagRunsAll, rErr := c.ReadDagRuns(dagId, topN)
+	if rErr != nil {
+		t.Fatalf("Error while reading all dag runs for %s: %s", dagId, rErr.Error())
+	}
+
+	if len(dagRunsAll) != topN {
+		t.Errorf("Expected %d dag runs, got %d", topN, len(dagRunsAll))
+		for _, dr := range dagRunsAll {
+			t.Logf("%+v", dr)
+		}
+	}
+
+	rows := len(timestamps)
+	for idx, dr := range dagRunsAll {
+		if dr.RunId != int64(rows-idx) {
+			t.Errorf("Expected RunId=%d, got %d", rows-idx, dr.RunId)
+		}
+		if dr.ExecTs != timestamps[rows-idx-1] {
+			t.Errorf("Expected ExecTs=%s, got: %s", timestamps[rows-idx-1], dr.ExecTs)
+		}
+		if dr.Status != DagRunStatusScheduled {
+			t.Errorf("Expected Status=%s, but got: %s", DagRunStatusScheduled, dr.Status)
+		}
+	}
+}
+
+func TestInsertAndReadDagRunsTop1000(t *testing.T) {
+	c, err := NewInMemoryClient(sqlSchemaPath)
+	if err != nil {
+		t.Error(err)
+	}
+	dagId := "mock_dag"
+	timestamps := []string{
+		"2023-09-23T10:10:00",
+		"2023-09-23T10:20:00",
+		"2023-09-23T10:30:00",
+		"2023-09-23T10:40:00",
+		"2023-09-23T10:50:00",
+		"2023-09-23T11:00:00",
+	}
+	for _, ts := range timestamps {
+		insertDagRun(c, dagId, ts, t)
+	}
+
+	// Read all dag runs for mock_dag
+	const topN = 1000
+	dagRunsAll, rErr := c.ReadDagRuns(dagId, topN)
+	if rErr != nil {
+		t.Fatalf("Error while reading all dag runs for %s: %s", dagId, rErr.Error())
+	}
+
+	if len(dagRunsAll) != len(timestamps) {
+		t.Errorf("Expected %d dag runs, got %d", len(timestamps), len(dagRunsAll))
+		for _, dr := range dagRunsAll {
+			t.Logf("%+v", dr)
+		}
+	}
+
+	rows := len(dagRunsAll)
+	for idx, dr := range dagRunsAll {
+		if dr.RunId != int64(rows-idx) {
+			t.Errorf("Expected RunId=%d, got %d", rows-idx, dr.RunId)
+		}
+		if dr.ExecTs != timestamps[rows-idx-1] {
+			t.Errorf("Expected ExecTs=%s, got: %s", timestamps[rows-idx-1], dr.ExecTs)
+		}
+		if dr.Status != DagRunStatusScheduled {
+			t.Errorf("Expected Status=%s, but got: %s", DagRunStatusScheduled, dr.Status)
+		}
+	}
+}
+
+func insertDagRun(c *Client, dagId, execTs string, t *testing.T) {
+	_, iErr := c.InsertDagRun(dagId, execTs)
+	if iErr != nil {
+		t.Errorf("Error while inserting dag run: %s", iErr.Error())
+	}
+}
