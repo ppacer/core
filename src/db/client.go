b615be6e598d1a21e90d98568e500d79177923a2
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-11-09 15:15:35 +0100

    Introduce SQLite DB client based on temp file and switch sync.Mutext to RWMutex
    Switching to sync.RWMutex was necessary to handle safely concurrent
    writes and reads (many reads but single write at the time) for SQLite DB
    file.
    
    Also there is a cleanup function which doesn't clean SQLite DB in case
    when tests has failed, so developer can debug the database side offline.

--- src/db/client.go
+++ src/db/client.go
@@ -7,12 +7,15 @@ import (
 	"log/slog"
 	"os"
 	"strings"
+	"testing"
 )
 
 type DB interface {
 	Begin() (*sql.Tx, error)
 	Exec(query string, args ...any) (sql.Result, error)
 	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	Close() error
+	DataSource() string
 	Query(query string, args ...any) (*sql.Rows, error)
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	QueryRow(query string, args ...any) *sql.Row
@@ -57,4 +60,64 @@ func NewInMemoryClient(schemaScriptPath string) (*Client, error) {
 	}
 	sqliteDB := SqliteDB{dbConn: db}
 	return &Client{&sqliteDB}, nil
+}
+
+// Produces new Client using SQLite database created as temp file. It's mainly
+// for testing and ad-hocs.
+func NewSqliteTmpClient(schemaScriptPath string) (*Client, error) {
+	schema, err := os.ReadFile(schemaScriptPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a temporary file to hold the SQLite database
+	tmpFile, err := os.CreateTemp("", "sqlite-")
+	if err != nil {
+		return nil, err
+	}
+	tmpFilePath := tmpFile.Name()
+	tmpFile.Close()
+
+	// Connect to the SQLite database using the temporary file path
+	db, err := sql.Open("sqlite", tmpFilePath)
+	if err != nil {
+		os.Remove(tmpFilePath)
+		return nil, err
+	}
+	execQueries := strings.Split(string(schema), ";")
+	for _, query := range execQueries {
+		query = strings.TrimSpace(query)
+		if query == "" {
+			continue
+		}
+		_, err = db.Exec(query)
+		if err != nil {
+			db.Close()
+			os.Remove(tmpFilePath)
+			return nil, err
+		}
+	}
+	sqliteDB := SqliteDB{dbConn: db, dbFilePath: tmpFilePath}
+	return &Client{&sqliteDB}, nil
+}
+
+// CleanUpSqliteTmp deletes SQLite database source file if all tests in the
+// scope passed. In at least one test failed, database will not be deleted, to
+// enable futher debugging. Even though this function takes generic *Client,
+// it's mainly meant for SQLite-based database clients which are used in
+// testing.
+func CleanUpSqliteTmp(c *Client, t *testing.T) {
+	if closeErr := c.dbConn.Close(); closeErr != nil {
+		t.Errorf("Error while closing connection to DB: %s", closeErr.Error())
+	}
+	if t.Failed() {
+		t.Logf("Database was not deleted. Please check: sqlite3 %s",
+			c.dbConn.DataSource())
+		return
+	}
+	// tests passed, we can proceed to remove DB file
+	if err := os.Remove(c.dbConn.DataSource()); err != nil {
+		t.Errorf("Cannot remove database source file %s: %s",
+			c.dbConn.DataSource(), err.Error())
+	}
 }
--- src/db/sqlite.go
+++ src/db/sqlite.go
@@ -9,8 +9,9 @@ import (
 )
 
 type SqliteDB struct {
-	sync.Mutex
-	dbConn *sql.DB
+	sync.RWMutex
+	dbConn     *sql.DB
+	dbFilePath string
 }
 
 func (s *SqliteDB) Begin() (*sql.Tx, error) {
@@ -23,24 +24,111 @@ func (s *SqliteDB) Exec(query string, args ...any) (sql.Result, error) {
 	return s.dbConn.Exec(query, args...)
 }
 
-func (s *SqliteDB) ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error) {
+func (s *SqliteDB) ExecContext(
+	ctx context.Context, query string, args ...any,
+) (sql.Result, error) {
 	s.Lock()
 	defer s.Unlock()
 	return s.dbConn.ExecContext(ctx, query, args...)
 }
 
+func (s *SqliteDB) Close() error {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.Close()
+}
+
+func (s *SqliteDB) DataSource() string {
+	return s.dbFilePath
+}
+
 func (s *SqliteDB) Query(query string, args ...any) (*sql.Rows, error) {
+	s.RLock()
+	defer s.RUnlock()
 	return s.dbConn.Query(query, args...)
 }
 
-func (s *SqliteDB) QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error) {
+func (s *SqliteDB) QueryContext(
+	ctx context.Context, query string, args ...any,
+) (*sql.Rows, error) {
+	s.RLock()
+	defer s.RUnlock()
 	return s.dbConn.QueryContext(ctx, query, args...)
 }
 
 func (s *SqliteDB) QueryRow(query string, args ...any) *sql.Row {
+	s.RLock()
+	defer s.RUnlock()
 	return s.dbConn.QueryRow(query, args...)
 }
 
-func (s *SqliteDB) QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row {
+func (s *SqliteDB) QueryRowContext(
+	ctx context.Context, query string, args ...any,
+) *sql.Row {
+	s.RLock()
+	defer s.RUnlock()
+	return s.dbConn.QueryRowContext(ctx, query, args...)
+}
+
+// SQLite database where data is stored in the memory rather than in a file on
+// a disk. It needs additional level of isolation for concurrent access.
+type SqliteDBInMemory struct {
+	sync.Mutex
+	dbConn *sql.DB
+}
+
+func (s *SqliteDBInMemory) Begin() (*sql.Tx, error) {
+	return s.dbConn.Begin()
+}
+
+func (s *SqliteDBInMemory) Exec(query string, args ...any) (sql.Result, error) {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.Exec(query, args...)
+}
+
+func (s *SqliteDBInMemory) ExecContext(
+	ctx context.Context, query string, args ...any,
+) (sql.Result, error) {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.ExecContext(ctx, query, args...)
+}
+
+func (s *SqliteDBInMemory) Close() error {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.Close()
+}
+
+func (s *SqliteDBInMemory) DataSource() string {
+	return "IN_MEMORY"
+}
+
+func (s *SqliteDBInMemory) Query(query string, args ...any) (*sql.Rows, error) {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.Query(query, args...)
+}
+
+func (s *SqliteDBInMemory) QueryContext(
+	ctx context.Context, query string, args ...any,
+) (*sql.Rows, error) {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.QueryContext(ctx, query, args...)
+}
+
+func (s *SqliteDBInMemory) QueryRow(query string, args ...any) *sql.Row {
+	s.Lock()
+	defer s.Unlock()
+	return s.dbConn.QueryRow(query, args...)
+}
+
+func (s *SqliteDBInMemory) QueryRowContext(
+	ctx context.Context, query string, args ...any,
+) *sql.Row {
+	s.Lock()
+	defer s.Unlock()
 	return s.dbConn.QueryRowContext(ctx, query, args...)
 }
