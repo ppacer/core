e8ff34518ae970ab7b58d135714278018ed2f5ec
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2023-11-13 22:36:20 +0100

    Reduce code duplication in task scheduler tests

--- src/sched/tasks_test.go
+++ src/sched/tasks_test.go
@@ -231,108 +231,6 @@ func TestWalkAndScheduleOnAsyncTasks(t *testing.T) {
 	wg.Wait()
 }
 
-/*
-// TODO: Go back to this test case when full task life cycle will be
-// implemented.
-
-// Testing walkAndSchedule on the following DAG using very small TaskQueue:
-//
-//	   n21
-//	 /     \
-//	/       \
-//
-// n1 -- n22 -- n3
-//
-//	\       /
-//	 \     /
-//	   n23
-func TestWalkAndScheduleOnAsyncTasksSmallQueue(t *testing.T) {
-	const taskQueueCapacity = 2
-	t.Logf("Preparing scenario with task queue capacity: %d", taskQueueCapacity)
-
-	ts := defaultTaskScheduler(t, taskQueueCapacity)
-	defer db.CleanUpSqliteTmp(ts.DbClient, t)
-	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
-	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
-	d := dag.New("mock_dag").AddSchedule(schedule).AddRoot(nodes131()).Done()
-	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
-	var wg sync.WaitGroup
-	wg.Add(5)
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelFunc()
-	alreadyMarkedTasks := ds.NewAsyncMap[DagRunTask, any]()
-	drtStart := DagRunTask{dagrun.DagId, dagrun.AtTime, "n1"}
-	drtN21 := DagRunTask{dagrun.DagId, dagrun.AtTime, "n21"}
-	drtN22 := DagRunTask{dagrun.DagId, dagrun.AtTime, "n22"}
-	drtN23 := DagRunTask{dagrun.DagId, dagrun.AtTime, "n23"}
-	drtN3 := DagRunTask{dagrun.DagId, dagrun.AtTime, "n3"}
-
-	t.Logf("Start ts.walkAndSchedule...")
-	ts.walkAndSchedule(ctx, dagrun, d.Root, d.TaskParents(), alreadyMarkedTasks, &wg)
-	time.Sleep(100 * time.Millisecond)
-
-	t.Logf("Manually mark n1 as success")
-	uErr := ts.UpsertTaskStatus(ctx, drtStart, Success)
-	if uErr != nil {
-		t.Errorf("Error while updating dag run task status: %s", uErr.Error())
-	}
-	time.Sleep(100 * time.Millisecond)
-
-	// At this point n1 should have status Success, and n21, n22 and n23 should
-	// be Scheduled.
-	testTaskCacheQueueTableSize(ts, 4, t)
-	testTaskStatusInCache(ts, drtN21, Scheduled, t)
-	if _, n3Exists := ts.TaskCache.Get(drtN3); n3Exists {
-		t.Errorf("Unexpectedly %+v exists in TaskCache before n21, n22, n23 finished",
-			drtN3)
-	}
-
-	t.Logf("Manually mark n22 (just one of three scheduled tasks at the same time) as success")
-	uErr = ts.UpsertTaskStatus(ctx, drtN22, Success)
-	if uErr != nil {
-		t.Errorf("Error while updating dag run task %v status: %s", drtN22,
-			uErr.Error())
-	}
-	time.Sleep(100 * time.Millisecond)
-
-	// n3 still should not be scheduled yet
-	testTaskCacheQueueTableSize(ts, 4, t)
-	testTaskStatusInCache(ts, drtN21, Scheduled, t)
-	testTaskStatusInDB(ts, drtN21, Scheduled, t)
-	testTaskStatusInCache(ts, drtN22, Success, t)
-	testTaskStatusInDB(ts, drtN22, Success, t)
-	if _, n3Exists := ts.TaskCache.Get(drtN3); n3Exists {
-		t.Errorf("Unexpectedly %+v exists in TaskCache before n21, n22, n23 finished",
-			drtN3)
-	}
-
-	t.Logf("Mark n21 and n23 (the rest of async tasks) as Success")
-	uErr = ts.UpsertTaskStatus(ctx, drtN21, Success)
-	if uErr != nil {
-		t.Errorf("Error while updating dag run task %v status: %s", drtN21,
-			uErr.Error())
-	}
-	uErr = ts.UpsertTaskStatus(ctx, drtN23, Success)
-	if uErr != nil {
-		t.Errorf("Error while updating dag run task %v status: %s", drtN23,
-			uErr.Error())
-	}
-	time.Sleep(100 * time.Millisecond)
-
-	// n3 should be scheduled now, n21, n22 and n23 are done
-	testTaskCacheQueueTableSize(ts, 5, t)
-	testTaskStatusInCache(ts, drtN21, Success, t)
-	testTaskStatusInDB(ts, drtN21, Success, t)
-	testTaskStatusInCache(ts, drtN22, Success, t)
-	testTaskStatusInDB(ts, drtN22, Success, t)
-	testTaskStatusInCache(ts, drtN23, Success, t)
-	testTaskStatusInDB(ts, drtN23, Success, t)
-	testTaskStatusInCache(ts, drtN3, Scheduled, t)
-	testTaskStatusInDB(ts, drtN3, Scheduled, t)
-	wg.Wait()
-}
-*/
-
 func testTaskCacheQueueTableSize(ts *taskScheduler, expectedCount int, t *testing.T) {
 	if ts.TaskCache.Len() != expectedCount {
 		t.Errorf("Expected %d tasks in the cache, got: %d", expectedCount,
@@ -350,14 +248,73 @@ func testTaskCacheQueueTableSize(ts *taskScheduler, expectedCount int, t *testin
 }
 
 func TestScheduleDagTasksSimple131(t *testing.T) {
-	const delay = 50 * time.Millisecond
-	ts := defaultTaskScheduler(t, 100) // cache and DB is empty at this point
-	defer db.CleanUpSqliteTmp(ts.DbClient, t)
 	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
 	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
 	d := dag.New("mock_dag").AddSchedule(schedule).AddRoot(nodes131()).Done()
-	taskNum := len(d.Flatten())
 	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
+	testScheduleDagTasksSingleDagrun(
+		d, dagrun, 50*time.Millisecond, 100, t,
+	)
+}
+
+func TestScheduleDagTasksLinkedListShort(t *testing.T) {
+	testScheduleDagTasksLinkedList(10, t)
+}
+
+func TestScheduleDagTasksLinkedListLong(t *testing.T) {
+	testScheduleDagTasksLinkedList(100, t)
+}
+
+func TestScheduleDagTasksNoTasks(t *testing.T) {
+	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
+	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
+	d := dag.New("mock_dag_no_tasks").
+		AddSchedule(schedule).
+		Done()
+	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
+	testScheduleDagTasksSingleDagrun(
+		d, dagrun, 50*time.Millisecond, 10, t,
+	)
+}
+
+func TestScheduleDagTasksSingleTask(t *testing.T) {
+	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
+	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
+	theOnlyNode := dag.Node{Task: EmptyTask{TaskId: "start"}}
+	d := dag.New("mock_dag_single_task").
+		AddSchedule(schedule).
+		AddRoot(&theOnlyNode).
+		Done()
+	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
+	testScheduleDagTasksSingleDagrun(
+		d, dagrun, 50*time.Millisecond, 10, t,
+	)
+}
+
+func testScheduleDagTasksLinkedList(size int, t *testing.T) {
+	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
+	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
+	d := dag.
+		New(dag.Id(fmt.Sprintf("mock_dag_ll_%d", size))).
+		AddSchedule(schedule).
+		AddRoot(nodesLinkedList(size)).
+		Done()
+	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
+	testScheduleDagTasksSingleDagrun(
+		d, dagrun, 10*time.Millisecond, size, t,
+	)
+}
+
+func testScheduleDagTasksSingleDagrun(
+	d dag.Dag,
+	dagrun DagRun,
+	delay time.Duration,
+	queueLength int,
+	t *testing.T,
+) {
+	ts := defaultTaskScheduler(t, queueLength)
+	defer db.CleanUpSqliteTmp(ts.DbClient, t)
+	taskNum := len(d.Flatten())
 	errsChan := make(chan taskSchedulerError)
 	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancelFunc()
@@ -383,101 +340,6 @@ func TestScheduleDagTasksSimple131(t *testing.T) {
 	if cnt != taskNum {
 		t.Errorf("Expected %d tasks with %s status, got: %d",
 			taskNum, Success, cnt)
-	}
-	cnt = ts.DbClient.CountWhere("dagruns", statusValue)
-	if cnt != 1 {
-		t.Errorf("Expected 1 successful dagrun %v, got: %d",
-			dagrun, cnt)
-	}
-}
-
-func TestScheduleDagTasksLinkedListShort(t *testing.T) {
-	testScheduleDagTasksLinkedList(10, t, 3*time.Second)
-}
-
-func TestScheduleDagTasksLinkedListLong(t *testing.T) {
-	testScheduleDagTasksLinkedList(100, t, 30*time.Second)
-}
-
-func TestScheduleDagTasksNoTasks(t *testing.T) {
-	const delay = 50 * time.Millisecond
-	ts := defaultTaskScheduler(t, 10) // cache and DB is empty at this point
-	defer db.CleanUpSqliteTmp(ts.DbClient, t)
-	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
-	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
-	d := dag.New("mock_dag_no_tasks").
-		AddSchedule(schedule).
-		Done()
-	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
-	errsChan := make(chan taskSchedulerError)
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelFunc()
-	addErr := dag.Add(d)
-	if addErr != nil {
-		t.Errorf("Cannot add mock_dag to the dag.registry: %s", addErr.Error())
-	}
-	_, iErr := ts.DbClient.InsertDagRun(
-		ctx, string(d.Id), timeutils.ToString(dagrun.AtTime),
-	)
-	if iErr != nil {
-		t.Errorf("Cannot insert dag run %v: %s", dagrun, iErr.Error())
-	}
-
-	go listenOnSchedulerErrors(errsChan, t)
-	go markSuccessAllTasks(ctx, ts, delay, t)
-	ts.scheduleDagTasks(ctx, dagrun, errsChan)
-	t.Log("Dag run is done!")
-
-	// Asssertions after the dag run is done
-	cnt := ts.DbClient.Count("dagruntasks")
-	if cnt != 0 {
-		t.Errorf("Expected no tasks in dagruntasks , got: %d", cnt)
-	}
-	statusValue := fmt.Sprintf("Status='%s'", Success)
-	cnt = ts.DbClient.CountWhere("dagruns", statusValue)
-	if cnt != 1 {
-		t.Errorf("Expected 1 successful dagrun %v, got: %d",
-			dagrun, cnt)
-	}
-}
-
-func TestScheduleDagTasksSingleTask(t *testing.T) {
-	const delay = 50 * time.Millisecond
-	ts := defaultTaskScheduler(t, 10) // cache and DB is empty at this point
-	defer db.CleanUpSqliteTmp(ts.DbClient, t)
-	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
-	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
-	theOnlyNode := dag.Node{Task: EmptyTask{TaskId: "start"}}
-	d := dag.New("mock_dag_single_task").
-		AddSchedule(schedule).
-		AddRoot(&theOnlyNode).
-		Done()
-	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
-	errsChan := make(chan taskSchedulerError)
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancelFunc()
-	addErr := dag.Add(d)
-	if addErr != nil {
-		t.Errorf("Cannot add mock_dag to the dag.registry: %s", addErr.Error())
-	}
-	_, iErr := ts.DbClient.InsertDagRun(
-		ctx, string(d.Id), timeutils.ToString(dagrun.AtTime),
-	)
-	if iErr != nil {
-		t.Errorf("Cannot insert dag run %v: %s", dagrun, iErr.Error())
-	}
-
-	go listenOnSchedulerErrors(errsChan, t)
-	go markSuccessAllTasks(ctx, ts, delay, t)
-	ts.scheduleDagTasks(ctx, dagrun, errsChan)
-	t.Log("Dag run is done!")
-
-	// Asssertions after the dag run is done
-	statusValue := fmt.Sprintf("Status='%s'", Success)
-	cnt := ts.DbClient.CountWhere("dagruntasks", statusValue)
-	if cnt != 1 {
-		t.Errorf("Expected 1 task with %s status, got: %d",
-			Success, cnt)
 	}
 	cnt = ts.DbClient.CountWhere("dagruns", statusValue)
 	if cnt != 1 {
@@ -546,55 +408,6 @@ func TestScheduleDagTasksSimple131TwoDagRuns(t *testing.T) {
 	cnt = ts.DbClient.CountWhere("dagruns", statusValue)
 	if cnt != 2 {
 		t.Errorf("Expected 2 successful dagruns, got: %d", cnt)
-	}
-}
-
-func testScheduleDagTasksLinkedList(
-	size int,
-	t *testing.T,
-	timeout time.Duration,
-) {
-	const delay = 10 * time.Millisecond
-	ts := defaultTaskScheduler(t, size) // cache and DB is empty at this point
-	defer db.CleanUpSqliteTmp(ts.DbClient, t)
-	var startTs = time.Date(2023, time.August, 22, 15, 0, 0, 0, time.UTC)
-	schedule := dag.FixedSchedule{Start: startTs, Interval: 30 * time.Second}
-	d := dag.
-		New(dag.Id(fmt.Sprintf("mock_dag_ll_%d", size))).
-		AddSchedule(schedule).
-		AddRoot(nodesLinkedList(size)).
-		Done()
-	dagrun := DagRun{DagId: d.Id, AtTime: schedule.Next(startTs)}
-	errsChan := make(chan taskSchedulerError)
-	ctx, cancelFunc := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancelFunc()
-	addErr := dag.Add(d)
-	if addErr != nil {
-		t.Errorf("Cannot add mock_dag to the dag.registry: %s", addErr.Error())
-	}
-	_, iErr := ts.DbClient.InsertDagRun(
-		ctx, string(d.Id), timeutils.ToString(dagrun.AtTime),
-	)
-	if iErr != nil {
-		t.Errorf("Cannot insert dag run %v: %s", dagrun, iErr.Error())
-	}
-
-	go listenOnSchedulerErrors(errsChan, t)
-	go markSuccessAllTasks(ctx, ts, delay, t)
-	ts.scheduleDagTasks(ctx, dagrun, errsChan)
-	t.Log("Dag run is done!")
-
-	// Asssertions after the dag run is done
-	statusValue := fmt.Sprintf("Status='%s'", Success)
-	cnt := ts.DbClient.CountWhere("dagruntasks", statusValue)
-	if cnt != size {
-		t.Errorf("Expected %d tasks with %s status, got: %d",
-			size, Success, cnt)
-	}
-	cnt = ts.DbClient.CountWhere("dagruns", statusValue)
-	if cnt != 1 {
-		t.Errorf("Expected 1 successful dagrun %v, got: %d",
-			dagrun, cnt)
 	}
 }
 
