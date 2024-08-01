2ce3a1a13fc30a55b73567438e56b75391aba5f1
DSkrzypiec <damian.j.skrzypiec@gmail.com>
2024-08-01 22:56:49 +0200

    Add generic helper function db.groupBy2

--- db/general.go
+++ db/general.go
@@ -75,3 +75,46 @@ func (c *Client) singleInt64(ctx context.Context, cntQuery string, args ...any) 
 		args, "duration", time.Since(start))
 	return count, nil
 }
+
+// Function groupBy2 executes usual SQL aggregation in form of:
+//
+// # SELECT Col1, F(Col2) FROM table GROUP BY Col1
+//
+// Query might be more complex, the only assumption is for it, to return two
+// columns of type K and V.
+func groupBy2[K comparable, V any](
+	ctx context.Context, dbConn DB, logger *slog.Logger, query string, args ...any,
+) (map[K]V, error) {
+	start := time.Now()
+	logger.Debug("Start groupBy2 query", "query", query, "args", args)
+	result := make(map[K]V)
+
+	rows, qErr := dbConn.QueryContext(ctx, query, args...)
+	if qErr != nil {
+		logger.Error("Cannot execute groupBy2 query", "query", query, "args",
+			args, "err", qErr.Error())
+	}
+	var key K
+	var value V
+
+	for rows.Next() {
+		select {
+		case <-ctx.Done():
+			// Handle context cancellation or deadline exceeded
+			logger.Warn("Context done while processing rows", "err", ctx.Err())
+			return nil, ctx.Err()
+		default:
+		}
+		scanErr := rows.Scan(&key, &value)
+		if scanErr != nil {
+			logger.Error("Error while scanning groupBy2 query results", "err",
+				scanErr.Error())
+			return nil, scanErr
+		}
+		result[key] = value
+	}
+
+	logger.Debug("Finished groupBy2 query", "query", query, "args", args,
+		"duration", time.Since(start))
+	return result, nil
+}
